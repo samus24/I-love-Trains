5f3d7023d2a30147e5342c64fc187997c8863131
iRocillo <irocillolanda@gmail.com>
2014-04-27 17:59:44 +0200

    Update Coordenada.java

--- Coordenada.java
+++ Coordenada.java
@@ -1,27 +1,37 @@
 package Entidad;
 
+/**
+*	Comprobado por Ignacio Rocillo
+* 	Sin errores
+*/
+
 public class Coordenada {
 	private int x;
 	private int y;
+	
 	public Coordenada(int _x, int _y){
 		x = _x;
 		y = _y;
 	}
+	
 	public String toString(){
 		return "( "+ x +" , "+ y + " )";
 	}
-	public int getCordenadaX(){
+	
+	public int getCoordenadaX(){
 		return x;
 	}
-	public int getCordenadaY(){
+	
+	public int getCoordenadaY(){
 		return y;
 	}
+	
 	public void setX(int x) {
 		this.x = x;
 	}
+	
 	public void setY(int y) {
 		this.y = y;
 	}
 	
-
 }
