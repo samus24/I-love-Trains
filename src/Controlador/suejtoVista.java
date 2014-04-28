5cb0f10b66201c3b80d1c8e20dcbdf4819463aef
chalvare <chalvare@ucm.es>
2014-04-28 12:13:15 +0200

    Update suejtoVista.java
    Código revisado

--- src/Controlador/suejtoVista.java
+++ src/Controlador/suejtoVista.java
@@ -1,38 +1,36 @@
 package def;
+/*
+Código revisado por Christian Álvarez
+Se han colocado las llaves, renombrar atributos correctamente
+*/
 
-public class suejtoVista 
-{
+
+public class suejtoVista {
 	// Atributos
-    tren misTrenes[];
-    señales misSeñales[];
+    Tren misTrenes[];
+    Signal misSeñales[];
 	//observadores misObservadores[]; mejor usar ArrayList
     ArrayList<Observador> misObservadores;
     // SOLO DEL MUNDO
 
     // Métodos
-    void anadir(Observer miObservador)// Añadir un observador a cierto sujeto
-    {
+    void anadir(Observer miObservador){// Añadir un observador a cierto sujeto
     	misObservadores.add(miObservador);
     }
-    void quitar(Observer miObservador) // Obvio, joder
-    {
+    void quitar(Observer miObservador){ // Obvio, joder
     	misObservadores.remove(miObservador);
     }
-    void notificar() // Recibir un update
-    {
+    void notificar(){ // Recibir un update
     	// Supongo que notificar a los observadores
     	for (Observador o : misObservadores) o.notificar();
     }
-    void actualizar() // Actualizar la información de un sujeto (método de un observador)
-    {
+    void actualizar(){ // Actualizar la información de un sujeto (método de un observador)
 	for (Observador o : misObservadores) o.actualizar();
     }
-    void getEstado() // Toma un estado de una entidad
-    {
+    void getEstado(){ // Toma un estado de una entidad
     	// ¿qué entidad?
     }
-    void setEstado() // Modifica el estado de una entidad
-    {
+    void setEstado(){ // Modifica el estado de una entidad
     	// ¿qué entidad?
     }
 }
