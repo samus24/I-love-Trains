1dc0b1001b191c0fb91a1c8ae3fc2d48415291c5
chalvare <chalvare@ucm.es>
2014-04-28 12:09:46 +0200

    Update adaptaVista.java
    Código revisado

--- src/Controlador/adaptaVista.java
+++ src/Controlador/adaptaVista.java
@@ -1,17 +1,17 @@
 package def;
+/*
+Código revisado por Christian Álvarez
+- Se han colocado las llaves.
+*/
 
-public class adaptaVista 
-{
-	 void adaptaTren(String miTren) // Recibe el String de la interfaz y lo transforma en datos legibles por el mundo (básicamente para el método creaTren())
-	 {
+public class adaptaVista {
+	 void adaptaTren(String miTren){ // Recibe el String de la interfaz y lo transforma en datos legibles por el mundo (básicamente para el método creaTren())
 		 // ¿qué formato tiene esa String?
 	 }
-	 void adaptaSeñal(String miSeñal) // Recibe el String de la interfaz y lo transforma en datos legibles por el mundo (básicamente para el método creaSeñal())
-	 {
+	 void adaptaSeñal(String miSeñal){ // Recibe el String de la interfaz y lo transforma en datos legibles por el mundo (básicamente para el método creaSeñal())
 		 // ¿qué formato tiene esa String?
 	 }
-	 void adaptaTrayecto(String miTrayecto) // Recibe el String de la interfaz y lo transforma en datos legibles por el mundo (básicamente para el método creaTrayecto())
-	 {
+	 void adaptaTrayecto(String miTrayecto){ // Recibe el String de la interfaz y lo transforma en datos legibles por el mundo (básicamente para el método creaTrayecto())
 		 // ¿qué formato tiene esa String?
 	 }
 }
