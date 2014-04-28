2223528be8e7f01a5c867bac4408eb29b327ce06
Jeff/Dani <jeffer1191@hotmail.com>
2014-04-28 12:26:22 +0200

    primera version

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,19 @@
-// crear main
+import Vista.Principal;
+import Modelo.Mundo;
+import Controlador.Controlador;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Mundo modelo = new Mundo();
+		Principal vista= new Principal();
+		Controlador controler= new Controlador(modelo, vista);
+		do{
+			// Ejecucion del programa; Vista en ejecucion			
+		}while(!modelo.isQuit());
+
+	}
+
+}
