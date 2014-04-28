94b34835f8589d8f19441bc9aae99ea6a6e094df
chalvare <chalvare@ucm.es>
2014-04-28 12:08:22 +0200

    Update adaptaMundo.java
    Código revisado.

--- src/Controlador/adaptaMundo.java
+++ src/Controlador/adaptaMundo.java
@@ -1,10 +1,13 @@
 package def;
 
-public class adaptaMundo 
-{
+/*
+Código revisado por Christian Álvarez
+Se han colocado las llaves
+*/
+
+public class adaptaMundo {
 	// Métodos
-     void adaptaMapa() // Adaptar el mapa del mundo con la información bruta (a chars), y enviarlo para que la vista pueda imprimirlo
-     {
+     void adaptaMapa(){ // Adaptar el mapa del mundo con la información bruta (a chars), y enviarlo para que la vista pueda imprimirlo
 		 // ¿de donde sale la información bruta?
      }
 }
